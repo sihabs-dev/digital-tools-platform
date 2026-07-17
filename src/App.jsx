8c2ba2a1131c9c4bf63f5d9897bcfe1014a28cda
sihabs-dev <mdsarks123@gmail.com>
2026-07-17 21:35:44 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-8xl text-green-400">hello</h1>
+      <button className="btn btn-secondary">subscribe</button>
+    </>
+  );
+}
+
+export default App;
